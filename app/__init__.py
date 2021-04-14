5a52435a32ea2fb3e0be926d3f632021a73377cb
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2021-04-13 22:13:37 -0300

    começo da estrutura

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, template_rendered
+from decouple import config
+import mysql.connector
+
+app = Flask(__name__)
+db = mysql.connector.connect(user = config('USER_DB'), passwd = config('PASSWD_DB'), db = config('DB'))
+sql = db.cursor()
+app.config.from_object('config')
+
+from .routes import index
---
+++ config.py
@@ -0,0 +1,10 @@
+DEBUG = True
+
+# app directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLE = True
+CSRF_SESSION_KEY = '13ffb936-c01d-4ff9-a421-4aa349c82c23'
+
+SECRET_KEY = '80e39d47-de8a-4663-90b1-4741120a2fb5'
